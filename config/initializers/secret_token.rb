39b3f53e9b78dec867beba52ac56f971a6578132
Dmitri Skjorshammer <dmitriskj@gmail.com>
2013-02-23 13:34:29 -0800

    Initial commit; boilerplate Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bmrtracker::Application.config.secret_token = 'f3cb0f890993ad3e3b14ad5b1047ff45d6df93bf87bacc55058f30c7106fa88badf3a84510117ba3497547e2790bd3d9fac8f2d84d9611227a7ac0dd2d6682cb'
