55041649662f13d0067ce3ae0ee4eedeb25166b8
Juan Pablo Escobar <juannpablo@gmail.com>
2013-02-23 14:58:18 -0800

    Added seeds importing from csv file

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130223220027) do
+ActiveRecord::Schema.define(:version => 20130223223955) do
 
   create_table "properties", :force => true do |t|
     t.string   "name"
@@ -27,6 +27,9 @@ ActiveRecord::Schema.define(:version => 20130223220027) do
     t.float    "longitude"
     t.datetime "created_at",     :null => false
     t.datetime "updated_at",     :null => false
+    t.integer  "total_units"
+    t.integer  "total_bmr"
+    t.string   "neighborhood"
   end
 
 end
