acd96fba4c954d05f2e9a10e8e1f90b8507eb6ce
Juan Pablo Escobar <juannpablo@gmail.com>
2013-02-23 15:44:46 -0800

    Added respond with at properties controller

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,12 +1,16 @@
 class PropertiesController < ApplicationController
 
+  respond_to :json, :html
+
   def index
     @properties = Property.all
+    respond_with @properties
   end
 
   # GET /property/1
   def show
     @property = Property.find(params[:id])
+    respond_with @property
   end
 
 end
