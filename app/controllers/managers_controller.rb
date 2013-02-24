58e08abb00741909b75319c5eb10aa80a239791c
Dmitri Skjorshammer <dmitriskj@gmail.com>
2013-02-23 16:50:06 -0800

    Added managers index path

--- app/controllers/managers_controller.rb
+++ app/controllers/managers_controller.rb
@@ -1,6 +1,11 @@
 class ManagersController < ApplicationController
   respond_to :json, :html
 
+  def index
+    @managers = Manager.all
+    respond_with @managers
+  end
+
   # GET /property/1
   def show
     @manager = Manager.find(params[:id])
